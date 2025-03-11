766eeb6de04adbc4b38430a835536b478e2b72b0
Victor Mugisha <victormugishavm6@gmail.com>
2025-03-11 21:23:32 +0200

    Updated Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,10 +5,10 @@ import Footer from "../components/Footer";
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col justify-between max-w-[2000px] mx-auto">
-      <>
+      <div>
         <Header />
         <Outlet />
-      </>
+      </div>
       <Footer />
     </div>
   );
