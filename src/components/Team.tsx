06bd4f2bdfde38b258326eef7b6a7c70ccfb0f4f
Victor Mugisha <victormugishavm6@gmail.com>
2025-03-14 17:26:33 +0200

    Extracted the team data to an external data file

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,41 +1,6 @@
+import { teamData } from "../data/teamData";
+
 export default function Team() {
-  const teamData = [
-    {
-      name: "Shania Keza",
-      role: "Team Leader",
-      image: "/profiles/kesha-profile.png",
-      firstColor: "from-slate-200",
-      secondColor: "to-slate-300",
-    },
-    {
-      name: "Chanella Keza",
-      role: "Marketing Manager",
-      image: "/profiles/chanella-profile.png",
-      firstColor: "from-blue-100",
-      secondColor: "to-blue-200",
-    },
-    {
-      name: "Victor Mugisha",
-      role: "Software Developer",
-      image: "/profiles/victor-profile.png",
-      firstColor: "from-gray-200",
-      secondColor: "to-gray-300",
-    },
-    {
-      name: "Grace Muzirankoni",
-      role: "Public Relations",
-      image: "/profiles/grace-profile.png",
-      firstColor: "from-emerald-50",
-      secondColor: "to-emerald-100",
-    },
-    {
-      name: "Boaz Prince Iza",
-      role: "Software Developer",
-      image: "/profiles/boaz-profile.png",
-      firstColor: "from-lime-50",
-      secondColor: "to-lime-100",
-    },
-  ];
   return (
     <section className="py-10 md:py-16 px-4 md:px-8 bg-white flex flex-col items-center justify-center">
       <div className="text-center w-full max-w-2xl flex flex-col gap-4">
---
+++ src/data/teamData.ts
@@ -0,0 +1,37 @@
+export const teamData = [
+  {
+    name: "Shania Keza",
+    role: "Team Leader",
+    image: "/profiles/kesha-profile.png",
+    firstColor: "from-slate-200",
+    secondColor: "to-slate-300",
+  },
+  {
+    name: "Chanella Keza",
+    role: "Marketing Manager",
+    image: "/profiles/chanella-profile.png",
+    firstColor: "from-blue-100",
+    secondColor: "to-blue-200",
+  },
+  {
+    name: "Victor Mugisha",
+    role: "Software Developer",
+    image: "/profiles/victor-profile.png",
+    firstColor: "from-gray-200",
+    secondColor: "to-gray-300",
+  },
+  {
+    name: "Grace Muzirankoni",
+    role: "Public Relations",
+    image: "/profiles/grace-profile.png",
+    firstColor: "from-emerald-50",
+    secondColor: "to-emerald-100",
+  },
+  {
+    name: "Boaz Prince Iza",
+    role: "Software Developer",
+    image: "/profiles/boaz-profile-2.png",
+    firstColor: "from-lime-50",
+    secondColor: "to-lime-100",
+  },
+];
